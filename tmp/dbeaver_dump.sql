e9821b985be91ce9dda9c9594a3daf294cd80c07
aadityakumthekar <aditya_kumthekar@persistent.com>
2023-05-26 17:47:09 +0530

    inital commit

---
+++ tmp/dbeaver_dump.sql
@@ -0,0 +1,8 @@
+INSERT INTO AdventureWorks.dbo.ActiveUserConnections (DBName,NumberOfConnections,LoginName) VALUES
+	 (N'master',3,N'CloudDbSqlRoot                                                                                                                  '),
+	 (N'msdb',3,N'NT AUTHORITY\NETWORK SERVICE                                                                                                    '),
+	 (N'master',29,N'sa                                                                                                                              '),
+	 (N'master',6,N'sqlserver                                                                                                                       '),
+	 (N'AdventureWorks',5,N'sqlserver                                                                                                                       '),
+	 (N'test',1,N'sqlserver                                                                                                                       '),
+	 (N'AdventureWorksOLTP',2,N'sqlserver                                                                                                                       ');
